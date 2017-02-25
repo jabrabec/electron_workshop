0d99e19579734c2d96f24a73007beaf163229b4f
Jennifer Brabec <jenniferbrabec@gmail.com>
2017-02-25 12:55:01 -0800

    button added to index.html for file opening (windows modification). code added to index.js for opening files in program window. renderer.js modified to handle file opening. style.css updated per https://github.com/felixrieseberg/electron-code-editor

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.on('ready', () => {
     mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file:///${index}`);
     // loadURL ('file:///c/Users/Jennifer/../index.html')
+    mainWindow.webContents.on('will-navigate', (e, url) => {
+        e.preventDefault();
+        console.log(`Tried to navigate to: ${url}`);
+
+        mainWindow.webContents.send('navigate', url);
+    });
 });
 
 // felixrieseberg/electron-code-editor
--- renderer.js
+++ renderer.js
@@ -1,4 +1,7 @@
-const loader = require('monaco-loader')
+const { ipcRenderer } = require('electron');
+const loader = require('monaco-loader');
+const fs = require('fs');
+const {dialog} = require('electron').remote;
 
 loader().then(monaco => {
   let editor = monaco.editor.create(document.getElementById('container'), {
@@ -6,4 +9,24 @@ loader().then(monaco => {
     theme: 'vs-dark',
     automaticLayout: true
   })
+
+  document.getElementById('open-button').onclick = () => {
+    dialog.showOpenDialog({}, (files) => {
+        if (files && files.length > 0) {
+            console.log(`Trying to read from ${files[0]}`);
+            fs.readFile(files[0], 'utf8', (error, result) => {
+                if (!error) {
+                    console.log(result);
+
+                    editor.setModel(monaco.editor.createModel(result, 'javascript'));
+                }
+            });
+        }
+    })
+  }
 })
+
+// ipcRenderer.on('navigate', (e, url) => {
+//     url = url.slice(7);
+    
+// })
