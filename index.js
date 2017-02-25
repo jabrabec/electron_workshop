16fb1d0078c11f7e52a0d982a2460fac9b0d4e51
Jennifer Brabec <jenniferbrabec@gmail.com>
2017-02-25 11:33:14 -0800

    index.js modified to load index.html instead of external website. pkg.json updated to reflect installation of monaco-loader. renderer.js, style.css, index.html initial commits

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+// to install:
+// npm install --save electron
+
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 let mainWindow;
 
 app.on('ready', () => {
+    const index = path.join(__dirname, 'index.html')
+    // eg /c/Users/Jennifer/../index.html
     mainWindow = new BrowserWindow({});
-    mainWindow.loadURL('http://www.google.com');
+    mainWindow.loadURL(`file:///${index}`);
+    // loadURL ('file:///c/Users/Jennifer/../index.html')
 });
+
+// felixrieseberg/electron-code-editor
